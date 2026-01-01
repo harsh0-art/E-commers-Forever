b578a8bf0fa236f720d8b1d7b2374a79b37b29cf
pegause0 <harshpatel994959@gmail.com>
2026-01-01 15:44:14 +0530

    first commit

---
+++ backend/routes/orderRoute.js
@@ -0,0 +1,34 @@
+import express from "express";
+
+import {
+  placeOrder,
+  placeOrderStrip,
+  placeOrderRazorpay,
+  allOrders,
+  userOrders,
+  updateStatus,
+  verifyStripe,
+} from "../controllers/orderController.js";
+
+import adminAuth from "../middlware/adminAuth.js";
+import authUser from "../middlware/auth.js";
+
+const orderRouter = express.Router();
+
+// Admin Features
+orderRouter.post("/list", adminAuth, allOrders);
+orderRouter.post("/status", adminAuth, updateStatus);
+
+//Payment Features
+orderRouter.post("/place", authUser, placeOrder);
+orderRouter.post("/stripe", authUser, placeOrderStrip);
+orderRouter.post("/razorpay", authUser, placeOrderRazorpay);
+
+//User Feature
+
+orderRouter.post("/userorders", authUser, userOrders);
+
+//verify Payment
+orderRouter.post("/verifyStripe", authUser, verifyStripe);
+
+export default orderRouter;
